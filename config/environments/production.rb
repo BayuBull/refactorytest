20766178f18e1abd712a6ed1cd3c3659e75e0ba7
BayuBull <bayuagungn@gmail.com>
2021-02-25 03:24:26 +0700

    add smtp on env production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "mail.google.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV.fetch("EMAIL"),
+    password: ENV.fetch("EMAIL_PASS")
+  }
 end
